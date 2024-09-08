f4b819fe9a79d03ac93b3be54c74ca3023124237
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-08 17:56:42 +0200

    app.py: add comments to organise code

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# Imports
 from flask import Flask, render_template, redirect, url_for
 from views.home import home
 from db import mysql
+
+# Main app
 app = Flask(__name__)
 
+# Database Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -10,11 +14,14 @@ app.config['MYSQL_DB'] = 'ksiegacytatow'
 
 mysql.init_app(app)
 
+# Blueprints
 app.register_blueprint(home, url_prefix='/home')
 
+# Main route
 @app.route('/')
 def index():
     return redirect(url_for('home.homepage'))
 
+# Run
 if __name__ == '__main__':
     app.run()
