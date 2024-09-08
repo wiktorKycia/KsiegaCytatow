efb79143a115a9ed387b4b56712185590667a868
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-08 15:17:45 +0200

    in /home path, return rendered template of index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>home page<h1/>'
+    return render_template('index.html', title='Home', content='Home')
 
 
 if __name__ == '__main__':
