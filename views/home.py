b6c492b3f26e895dd6285b163bd7b8df984fc906
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-08 17:45:43 +0200

    import db to home.py views, establish a /database route, that simply grabs the data from 'users' table and passes it to data.html document

--- views/home.py
+++ views/home.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
-
+from db import mysql
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def homepage():
     return render_template('home/index.html', title='Home', content='home')
+
+@home.route('/database')
+def database():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    cursor.close()
+    return render_template('home/data.html', data=users)
