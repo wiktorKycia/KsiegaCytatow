eab994a90eb63e1325e7b304f0d905a600a82839
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-08 17:07:49 +0200

    in home.py rename the function to homepage; in app.py import home blueprint as home; correct the url_for in index

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
-from views.home import home as home_blueprint
+from views.home import home
 app = Flask(__name__)
-app.register_blueprint(home_blueprint, url_prefix='/home')
+app.register_blueprint(home, url_prefix='/home')
 
 @app.route('/')
 def index():
-    return redirect(url_for('home_blueprint.home'))
-
+    return redirect(url_for('home.homepage'))
 
 if __name__ == '__main__':
     app.run()
--- views/home.py
+++ views/home.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 home = Blueprint('home', __name__)
 
 @home.route('/')
-def home():
+def homepage():
     return render_template('index.html', title='Home', content='home')
