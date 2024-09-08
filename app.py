50cd44b995c88ba019adb139f35eed13cf38e573
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-08 17:16:59 +0200

    import flask_mysqldb package and set up some config variables required for working with a MySQL database

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 from views.home import home
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'ksiegacytatow'
 app.register_blueprint(home, url_prefix='/home')
 
 @app.route('/')
