68fb76f160f0dc78aefe4b254f7c4b2ff90a4459
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-08 16:51:16 +0200

    to app.py import home as home_blueprint, in index func: redirect for home_blueprint.home

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
-from views.home import home
+from views.home import home as home_blueprint
 app = Flask(__name__)
-app.register_blueprint(home, url_prefix='/home')
+app.register_blueprint(home_blueprint, url_prefix='/home')
 
 @app.route('/')
 def index():
-    return redirect(url_for('home/'))
+    return redirect(url_for('home_blueprint.home'))
 
 
 if __name__ == '__main__':
