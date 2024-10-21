917d655faa9bc255b21075b2bed2dc3baace1e19
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-10-21 12:22:27 +0200

    create the functions for token creation and verification in app.py

--- app.py
+++ app.py
@@ -53,6 +53,25 @@ app.register_blueprint(home, url_prefix='/home')
 app.register_blueprint(admin, url_prefix='/admin')
 app.register_blueprint(profile, url_prefix='/profile/<user_url_slug>')
 
+
+
+def generate_verification_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+def verify_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration  # Token expires after 1 hour
+        )
+    except:
+        return False
+    return email
+
+
 # Main route
 @app.route('/')
 def index():
