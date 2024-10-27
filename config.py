56ca15dada2dc4ebcf0a1d8709773026ad951df6
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-10-27 18:08:24 +0100

    update functions for sending verification email and change password email

--- config.py
+++ config.py
@@ -40,22 +40,17 @@ def send_email(email_to:str, subject:str, body_text:str, route:str):
     mail.send(msg)
 
 def send_verification_email(user_email):
-    token = generate_verification_token(user_email)
-    verification_url = url_for('home.verify_email', token=token, _external=True)
-    subject = "Please verify your email".encode('utf-8')
-    body = f"Click the link to verify your email: {verification_url}".encode('utf-8')
-
-    # Send the email
-    msg = Message(subject=subject.decode('utf-8'), recipients=[user_email], body=body.decode('utf-8'))
-    msg.charset = 'utf-8'  # Set the encoding explicitly
-    mail.send(msg)
+    send_email(
+        email_to=user_email,
+        subject="Please verify your email",
+        body_text="Click the link to verify your email: {url}",
+        route='home.verify_email'
+    )
 
 def send_change_password_email(user_email):
-    token = generate_verification_token(user_email)
-    url = url_for('profile.change_password', token=token, _external=True)
-    subject = "Password change".encode('utf-8')
-    body = f"Click the link to change your password: {url}".encode('utf-8')
-
-    msg = Message(subject=subject.decode('utf-8'), recipients=[user_email], body=body.decode('utf-8'))
-    msg.charset = 'utf-8'
-    mail.send(msg)
+    send_email(
+        email_to=user_email,
+        subject="Password change",
+        body_text="Click the link to change your password: {url}",
+        route='profile.change_password'
+    )
