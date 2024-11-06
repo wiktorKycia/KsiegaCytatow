42690033a7dee2fc7ec188b1ba4620472bfd6652
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-11-06 11:28:24 +0100

    add an important sql query, that is vital in the 2nd section of the plan in a comment

--- zapytania.sql
+++ zapytania.sql
@@ -14,4 +14,12 @@ select *
 from  (SELECT user_id, author_id, NULL as 'nickname_id' FROM (
         SELECT u.`id` as 'user_id', a.`id` as 'author_id' FROM `authors` a JOIN `users` u
     ) AS tbl
-    ORDER BY user_id, author_id) alias
+    ORDER BY user_id, author_id) alias;
+
+-- autorzy i ich nicki wybrane przez użytkownika o id = 2
+SELECT
+unp.`user_id`, unp.`author_id`, unp.`nickname_id`, CONCAT(a.first_name, ' ', a.last_name) as 'author_name'  , n.nick
+FROM `usernicknamespreferences` unp
+JOIN `authors` a ON a.id = unp.author_id
+LEFT JOIN `nicknames` n ON n.id = unp.nickname_id
+WHERE unp.user_id = 2
