41ff2b8d0272373631a7e0a9e09b07e5b962a739
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-11-06 11:37:12 +0100

    shorten the number of columns of the query, leave only the vital ones

--- zapytania.sql
+++ zapytania.sql
@@ -18,7 +18,7 @@ from  (SELECT user_id, author_id, NULL as 'nickname_id' FROM (
 
 -- autorzy i ich nicki wybrane przez użytkownika o id = 2
 SELECT
-unp.`user_id`, unp.`author_id`, unp.`nickname_id`, CONCAT(a.first_name, ' ', a.last_name) as 'author_name'  , n.nick
+CONCAT(a.first_name, ' ', a.last_name) as 'author_name'  , n.nick
 FROM `usernicknamespreferences` unp
 JOIN `authors` a ON a.id = unp.author_id
 LEFT JOIN `nicknames` n ON n.id = unp.nickname_id
